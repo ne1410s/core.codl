904d976f11f7f5cef6dd75516fefbf01984ea782
ne1410s <pgj646@gmail.com>
2020-05-26 21:26:15 +0100

    Comment added

--- src/decorator/base.ts
+++ src/decorator/base.ts
@@ -1,4 +1,17 @@
 export class Base {
 
+  // todo: err yeah scrap this class!
+  // SEE: https://www.typescriptlang.org/docs/handbook/decorators.html
+  
+  // The idea of this library is to provide a component model style library
+  // so that decorators can be used to define properties, classes, etc in order
+  // to facilitate automated and/or generic data concerns such as:
+  //  - validation
+  //  - labelling
+  //  - ui controls,
+  //  - etc etc
+
+  // Should simplify tabl8r element too :)
+
   test: string;
 }
