6ee73fc4989058b40d9ae297a1b389fe905b01f5
Paul Jones <pjjones@siemens.com>
2020-06-02 14:44:50 +0100

    property wip

--- src/decorator/property.ts
+++ src/decorator/property.ts
@@ -1,1 +1,16 @@
-export const deleteme_prop = true;
+import 'reflect-metadata';
+
+const METAKEY_NAME = 'ne-codl_name';
+const METAKEY_DESCRIPTION = 'ne-codl_description';
+
+/**
+ * A friendly name to associate.
+ * @param val The name.
+ */
+export const name = (val: string) => Reflect.metadata(METAKEY_NAME, val);
+
+/**
+ * A description to associate.
+ * @param val The description.
+ */
+export const description = (val: string) => Reflect.metadata(METAKEY_DESCRIPTION, val);
