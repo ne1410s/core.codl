f908d83087d47463708b235a815e6cc03f223c8e
ne1410s <pgj646@gmail.com>
2020-06-25 08:58:19 +0100

    Minor restructure

--- src/reflect/validation/validation.ts
+++ src/reflect/validation/validation.ts
@@ -11,22 +11,20 @@ import { LengthRangeValidator } from './validators/length-range';
 /** Reflects validation decoration. */
 export abstract class ReflectValidation {
 
+  /**
+   * Performs validation - for objects with decorated members.
+   * @param target The object to validate.
+   * @param type The strong type (to supply prototype data).
+   */
   public static validate<T extends Object>(target: Object, type?: Ctor<T>): ValidationSummary {
+
     const proto = type?.prototype || Object.getPrototypeOf(target || {});
     if (!proto) throw new TypeError('No type data could be found');
-    const validators = this.getValidators();
-    const tests = ReflectValidation.buildTestPlan(validators, proto, target);
-    const results: ValidationResult[] = tests.map(t => {
-      return { navkey: t.navkey, tests: t.tests, ...t.fn(t.trg, t.key, t.proto) };
-    });
-    const retVal: ValidationSummary = { valid: results.every(r => r.valid) };
-    if (!retVal.valid) {
-      retVal.errors = results.filter(r => !r.valid).reduce((acc, cur) => {
-        acc[cur.navkey] = acc[cur.navkey] || [];
-        acc[cur.navkey].push(cur.message);
-        return acc;
-      }, {} as Record<string, string[]>)
-    }
+
+    const checks = this.getValidators();
+    const tests = this.buildTestPlan(checks, proto, target);
+    const results = this.executeTestPlan(tests);
+    const retVal = this.summarise(results);
 
     return retVal;
   }
@@ -35,12 +33,12 @@ export abstract class ReflectValidation {
    * Gets all available tests and their associated validator. There is expected
    * duplication where one validator is used to perform multiple tests.
    */
-  private static getValidators() {
+  private static getValidators(): ValidatorDef[] {
     return Object.keys(ValidationKey).map(test => {
       const meta = (ValidationKey as any)[test] as ValidationKey;
       const fn = ReflectValidation.getValidator(meta);
       return { fn, test, meta };
-    });  
+    });
   }
 
   /** Gets the validator function for a given key. */
@@ -64,11 +62,12 @@ export abstract class ReflectValidation {
     }
   }
 
-  private static buildTestPlan(dupeDefs: ValidatorDef[], proto: any, trg: Object, pfx: string = ''): ValidationInstruction[] {  
+  /** Prepares a sequence of validation instructions. */
+  private static buildTestPlan(allChecks: ValidatorDef[], proto: any, trg: Object, pfx: string = ''): ValidationInstruction[] {  
     return Reflect.getMetadataKeys(proto)
       .map(k => `${k}`)
       .reduce((acc, cur) => {
-        const valDef = dupeDefs.filter(dg => cur.indexOf(dg.meta + ':') === 0)[0];  
+        const valDef = allChecks.filter(dg => cur.indexOf(dg.meta + ':') === 0)[0];  
         if (valDef) {
           const key = cur.replace(valDef.meta + ':', '');
           const prior = acc.filter(a => a.key === key && a.fn === valDef.fn)[0];
@@ -83,15 +82,37 @@ export abstract class ReflectValidation {
           if (Array.isArray(subobj)) {
             subobj.forEach((subitem, i) => {
               const subpfx = pfx ? `${pfx}.${subkey}[${i}]` : `${subkey}[${i}]`;
-              acc.push(...ReflectValidation.buildTestPlan(dupeDefs, subproto, subitem || {}, subpfx));
+              acc.push(...ReflectValidation.buildTestPlan(allChecks, subproto, subitem || {}, subpfx));
             });
           } else if (subobj) {
             const subpfx = pfx ? `${pfx}.${subkey}` : subkey;
-            acc.push(...ReflectValidation.buildTestPlan(dupeDefs, subproto, subobj, subpfx));
+            acc.push(...ReflectValidation.buildTestPlan(allChecks, subproto, subobj, subpfx));
           }
         }
         return acc;
       }, [] as ValidationInstruction[])
       .sort((a, b) => a.navkey.split('.').length - b.navkey.split('.').length)
+  }
+
+  /** Executes tests, mapping to the result. */
+  private static executeTestPlan(tests: ValidationInstruction[]): ValidationResult[] {
+    return tests.map(t => {
+      const output = t.fn(t.trg, t.key, t.proto);
+      return { navkey: t.navkey, tests: t.tests, ...output };
+    });
+  }
+
+  /** Summarises a sequence of results. */
+  private static summarise(results: ValidationResult[]): ValidationSummary {
+    const retVal: ValidationSummary = { valid: results.every(r => r.valid) };
+    if (!retVal.valid) {
+      retVal.errors = results.filter(r => !r.valid).reduce((acc, cur) => {
+        acc[cur.navkey] = acc[cur.navkey] || [];
+        acc[cur.navkey].push(cur.message);
+        return acc;
+      }, {} as Record<string, string[]>)
+    }
+
+    return retVal;
   }
 }
