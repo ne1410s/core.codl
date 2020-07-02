b473c861a4398b97093682e9a17455b19e946a2d
ne1410s <pgj646@gmail.com>
2020-07-02 11:29:06 +0100

    Omit test output in main build

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -1,5 +1,12 @@
+import typescript from 'rollup-plugin-typescript2';
 import * as basis from './rollup.browser.config';
 
 basis.default.input = 'test/src/index.ts';
 basis.default.output.file += '.test.js';
+basis.default.plugins = [
+  typescript({
+    include: ['src/**/*.ts', 'test/src/**/*.ts'],
+  }),
+];
+
 export default basis.default;
